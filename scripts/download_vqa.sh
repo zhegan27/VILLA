1d5bc2bdbff301f5171385f1f47150450101a782
Linjie Li <linjieli222@gmail.com>
2021-01-12 22:08:32 -0800

    update download script for best vqa model

--- scripts/download_vqa.sh
+++ scripts/download_vqa.sh
@@ -14,12 +14,34 @@ BLOB='https://convaisharables.blob.core.windows.net/uniter'
 # image dbs
 for SPLIT in 'train2014' 'val2014' 'test2015'; do
     if [ ! -d $DOWNLOAD/img_db/coco_$SPLIT ] ; then
+        echo "Downloading default coco_$SPLIT ..."
         wget $BLOB/img_db/coco_$SPLIT.tar -P $DOWNLOAD/img_db/
         tar -xvf $DOWNLOAD/img_db/coco_$SPLIT.tar -C $DOWNLOAD/img_db
+    else
+        echo "Found default coco_$SPLIT, skipping ..."
+    fi
+    if [ ! -f $DOWNLOAD/img_db/coco_$SPLIT/nbb_th0.075_max100_min10.json ] ; then
+        echo "Downloading coco_$SPLIT with conf 0.075..."
+        wget $BLOB/img_db/coco_${SPLIT}_0.075.tar -P $DOWNLOAD/img_db/
+        tar -xvf $DOWNLOAD/img_db/coco_${SPLIT}_0.075.tar -C $DOWNLOAD/img_db
+    else
+        echo "Found coco_$SPLIT with conf 0.075, skipping ..."
     fi
 done
-wget $BLOB/img_db/vg.tar -P $DOWNLOAD/img_db/
-tar -xvf $DOWNLOAD/img_db/vg.tar -C $DOWNLOAD/img_db
+if [ ! -d $DOWNLOAD/img_db/vg ] ; then 
+    echo "Downloading default vg ..."
+    wget $BLOB/img_db/vg.tar -P $DOWNLOAD/img_db/
+    tar -xvf $DOWNLOAD/img_db/vg.tar -C $DOWNLOAD/img_db
+else
+    echo "Found default vg, skipping ..."
+fi
+if [ ! -f $DOWNLOAD/img_db/vg/nbb_th0.075_max100_min10.json ] ; then
+    echo "Downloading vg with conf 0.075..."
+    wget $BLOB/img_db/vg_0.075.tar -P $DOWNLOAD/img_db/
+    tar -xvf $DOWNLOAD/img_db/vg_0.075.tar -C $DOWNLOAD/img_db
+else
+    echo "Found vg with conf 0.075, skipping ..."
+fi
 
 # text dbs
 for SPLIT in 'train' 'trainval' 'devval' 'test' 'vg'; do
